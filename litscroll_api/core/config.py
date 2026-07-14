cde9072ad9280aad34ca778e0bbd69684541ee14
Tymur <bagdtim1049@gmail.com>
2026-07-14 12:37:03 +0300

    Update config.py

--- litscroll_api/core/config.py
+++ litscroll_api/core/config.py
@@ -7,9 +7,7 @@ class Settings(BaseSettings):
     debug: bool = False
     database_url: str
 
-    model_config = SettingsConfigDict(
-        case_sensitive=False, extra='ignore', env_file='.env.dev'
-    )
+    model_config = SettingsConfigDict(case_sensitive=False, extra='ignore')
 
 
 @lru_cache
